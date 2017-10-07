d57c95886ad79a30f9f9adfc61ba44268b9d633b
Mark Aves <markaves@gmail.com>
2017-10-07 23:02:33 +0800

    Fixed YTSearch mispelled and mispelled Videos

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,7 +3,7 @@ import React from 'react';
 const VideoList = (props) => {
   return (
     <ul className="col-md-4 list-group">
-      {props.video.length}
+      {props.videos.length}
     </ul>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import TYSearch from 'youtube-api-search';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyC3sc5GjkZgN_0luE6Xb82L8X-QSdPCH-k';
